97b7aecdf9685b5ca004c049d1d1de6fe7c091a5
hilarybrown <30668841+hilarybrown@users.noreply.github.com>
2017-09-07 14:02:18 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 console.log("Up and running!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
+
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId])  // displays which card flipped
+
+  cardsInPlay.push(cards[cardId]) 
+
+  if (cardsInPlay.length === 2) {  // verify 2 cards in play
+    checkForMatch();
+  }
+}
+
+flipCard(0);
+flipCard(2);
