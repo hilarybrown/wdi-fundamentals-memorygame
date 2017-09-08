0b403c804f68cbc43080944378b8d4a127f72ccf
hilarybrown <30668841+hilarybrown@users.noreply.github.com>
2017-09-08 00:50:13 -0400

    Add files via upload
    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 console.log("Up and running!")
 
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-
-// function for selecting or flipping over a card
-var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId])  // displays which card flipped
-
-  cardsInPlay.push(cards[cardId]) 
-
-  if (cardsInPlay.length === 2) {  // verify 2 cards in play
-    checkForMatch();
-  }
+//card faces and suits
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
 }
+];
 
+var cardsInPlay = [];
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
@@ -22,5 +33,28 @@ var checkForMatch = function() {
   }
 }
 
-flipCard(0);
-flipCard(2);
+
+// function for selecting or flipping over a card
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id'); //gets data-id attribute of card clicked to store in variable cardID
+  console.log("User flipped " + cards[cardId].rank)  // displays rank of card flipped
+  console.log(cards[cardId].suit); //shows suit of card flipped
+  this.setAttribute('src', cards[cardId].cardImage); 
+  cardsInPlay.push(cards[cardId].rank) 
+  if (cardsInPlay.length === 2) {  // verify 2 cards in play
+    checkForMatch();
+  }
+};
+
+//adding function to create the board
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {   // for loop in order to loop through the cards array
+	var cardElement = document.createElement('img') ; //created imgh element to store in valiable cardElement
+	cardElement.setAttribute("src", "images/back.png"); // sets img source to images folder
+	cardElement.setAttribute('data-id', i); 
+	cardElement.addEventListener('click', flipCard) // triggers flipCard function upon click
+	document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+
+createBoard(); // this calls the createBoard function
